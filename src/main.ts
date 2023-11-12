9dd86c3dc5d5e8978d96cc6bad274e0ad050a51a
Tran Duy Tuan Anh <tranduytuananh203@gmail.com>
2023-11-12 14:30:22 +0700

    add

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);  
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.PORT);
+  console.log("Listen to http://localhost:" + process.env.PORT);
 }
 bootstrap();
